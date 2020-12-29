a8ab4f0f30aaa95535ccfb06ce7bea0e60f0d5a0
Rahul Hardiyat <49034362+RahulHardiyat@users.noreply.github.com>
2020-12-29 23:53:43 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+/**
+ * Class ini unutk menjalankan applikasi
+ * 
+ * @author (Rahul Hardiyat) 
+ * @version (29 Desember 2020)
+ */
+public class Main{
+    public static void main(String[] args) {
+        Kalkulator calc = new Kalkulator();
+        calc.pack();
+    }
+}
